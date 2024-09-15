829068191d00665f36593afde577f8057a7bfe3d
umar-developer-01 <umarkhandev9@gmail.com>
2024-09-15 16:16:13 +0530

    Added Prisma and migrations

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
 
 export default function Home() {
   return (
---
+++ packages/db/index.ts
@@ -0,0 +1,18 @@
+export * from "@prisma/client"
+
+
+
+//command for installing 
+
+//Install Prisma CLI
+// npm install prisma --save-dev
+
+//Initialize Prisma:
+// npx prisma init
+
+
+
+// npm init -y
+// npx tsc --init
+
+// npx prisma migrate
