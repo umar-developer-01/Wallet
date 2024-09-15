0f68ed38215fda70e73e0f2472f810c8fce9d057
umar-developer-01 <umarkhandev9@gmail.com>
2024-09-15 14:21:52 +0530

    Setup the project

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+  <div className={`text-xl`}>
+     Work
+   </div>
+  );
+}
